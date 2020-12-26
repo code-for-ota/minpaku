92cf11268ed84f06d903f103e56695230d321f5d
tori-takashi <tori.toutoshi@gmail.com>
2020-12-26 17:22:07 +0900

    add routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'minpaku#index'
+
+  get 'survey' => 'survey#index'
+  post 'survey/answer' => 'survey#answer'
 end
