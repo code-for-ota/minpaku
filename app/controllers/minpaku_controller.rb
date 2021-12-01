cd47475977cc9d13f2d7c675774c2daf8f6dbd6c
tori-takashi <tori.toutoshi@gmail.com>
2021-12-01 14:41:12 +0900

    set cors to controller

--- app/controllers/minpaku_controller.rb
+++ app/controllers/minpaku_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class MinpakuController < ApplicationController
+  after_action :allow_iframe, only: [:index, :get_aggregate_values, :get_comments]
+
   def index
     @minpaku_list = Minpaku.all
     @survey_list = SurveyList.all
@@ -14,5 +16,12 @@ class MinpakuController < ApplicationController
 
   def get_comments
     render :json => Minpaku.find(request[:id]).get_comments
+  end
+
+  private
+  def allow_iframe
+    url="http://codeforota.tokyo"
+    response.headers['X-Frame-Options'] = "ALLOW-FROM #{url}"
+    response.headers['Content-Security-Policy'] = "frame-ancestors #{url}"
   end
 end
